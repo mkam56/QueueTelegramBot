9f0cdcb61687832e153e42d4aaa3fe8f49eb98df
Nergal <pudzhari@inbox.ru>
2024-11-03 20:07:59 +0300

    stupid redis

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ import logging
 import sys
 from os import getenv
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
+from redis import Redis
 from comands import main_router
 
 
-bot = Bot(token=getenv('BOT_TOKEN'))
+# redis.acl_setuser(username=getenv("REDIS_USER", default=""), password=getenv("REDIS_USER_PASSWORD", default=""))
+
 
 # Запуск бота
 async def main():
     try:
-        dp = Dispatcher()
+        # storage = RedisStorage(Redis(
+        #     host=getenv("REDIS_HOST", default="localhost"),
+        #     port=getenv("REDIS_PORT", default=6379),
+        #     db=getenv("REDIS_DB", default=0),
+        #     password=getenv("REDIS_PASSWORD", default=""),
+        # ))
+
+        storage = MemoryStorage()
+
+        dp = Dispatcher(storage=storage)
+
+        bot = Bot(token=getenv('BOT_TOKEN'))
+
         dp.include_router(main_router)
 
+        await bot.delete_webhook(drop_pending_updates=True) # пропускаем все накопленные сообщения.
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
